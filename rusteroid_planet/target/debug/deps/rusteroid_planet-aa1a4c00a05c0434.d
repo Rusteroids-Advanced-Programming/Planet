3adbd484fc7fdbfc1b87ce4ce7d83c64b8003b67
francesco-unitn <francesco.verdolin@studenti.unitn.it>
2026-01-02 17:18:58 +0100

    Modified Cargo.toml

--- rusteroid_planet/target/debug/deps/rusteroid_planet-aa1a4c00a05c0434.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Francesco\Desktop\advanced programming\Project\Project\Rusteroid_planet\rusteroid_planet\target\debug\deps\rusteroid_planet-aa1a4c00a05c0434.d: src\lib.rs
-
-C:\Users\Francesco\Desktop\advanced programming\Project\Project\Rusteroid_planet\rusteroid_planet\target\debug\deps\librusteroid_planet-aa1a4c00a05c0434.rmeta: src\lib.rs
-
-src\lib.rs:
